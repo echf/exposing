87ab667624eaa1e984e8eea61132a80f7d1feea9
echf <75340644+echf@users.noreply.github.com>
2024-07-24 19:36:20 +0100

    Update snow.js

--- assets/js/snow.js
+++ assets/js/snow.js
@@ -5,6 +5,11 @@ document.addEventListener('DOMContentLoaded', function() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
+    canvas.style.position = 'fixed';
+    canvas.style.top = '0';
+    canvas.style.left = '0';
+    canvas.style.zIndex = '-1'; 
+
     const snowflakes = [];
 
     function createSnowflakes() {
