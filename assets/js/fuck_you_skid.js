7bd76ba93f279dc3bfc587a886e83f82f64aba95
unk0wnusername <163967610+unk0wnusername@users.noreply.github.com>
2024-11-21 01:09:13 +0000

    Update fuck_you_skid.js

--- assets/js/fuck_you_skid.js
+++ assets/js/fuck_you_skid.js
@@ -1,1 +1,13 @@
+(function() {
+  const devTools = /./;
+  devTools.toString = function() {
+    return 'devtools';
+  };
 
+  setInterval(function() {
+    if (console.log.toString().indexOf('devtools') !== -1) {
+      alert("Developer tools are not allowed on this site.");
+      window.location.href = 'about:blank';  // Redirects the user to a blank page
+    }
+  }, 1000);  // Checks every second
+})();
